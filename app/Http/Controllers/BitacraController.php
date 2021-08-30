74f0d0bdfd4406c3be860071b6a31c0fa997a744
GC17026 <GC17026@ues.edu.sv>
2021-08-29 22:03:56 -0600

    migraciones, modelos y controladores

---
+++ app/Http/Controllers/BitacraController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Bitacra;
+use Illuminate\Http\Request;
+
+class BitacraController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Bitacra  $bitacra
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Bitacra $bitacra)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Bitacra  $bitacra
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Bitacra $bitacra)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Bitacra  $bitacra
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Bitacra $bitacra)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Bitacra  $bitacra
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Bitacra $bitacra)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/SubSeccionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\SubSeccion;
+use Illuminate\Http\Request;
+
+class SubSeccionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\SubSeccion  $subSeccion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SubSeccion $subSeccion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\SubSeccion  $subSeccion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SubSeccion $subSeccion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\SubSeccion  $subSeccion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SubSeccion $subSeccion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\SubSeccion  $subSeccion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SubSeccion $subSeccion)
+    {
+        //
+    }
+}
