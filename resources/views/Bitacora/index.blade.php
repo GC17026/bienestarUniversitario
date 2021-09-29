8f3bbe968c612c367e299efc8d9fe1ab43fd94e7
Concepción Orellana <og17007@ues.edu.sv>
2021-09-28 20:46:04 -0600

    Vista de bitácora maquetada

--- resources/views/Bitacora/index.blade.php
+++ resources/views/Bitacora/index.blade.php
@@ -2,22 +2,42 @@
 
 @section('homeContent')
 
-<div id="page-content-wrapper" class="w-100">
-    <div class="container w-100">
-        <div class="row justify-content-center">
-            <div class="">
-                <div class="card">
-                    <div class="card-header">{{ __('Seccion mamalona') }}</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                        @endif
-
-                        {{ __('You are logged in!') }}
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header ">
+                <div class="row">
+                    <div class="col-sm-11 text-center">
+                        <h2 class="card-title"><b>Bitácora de acciones realizadas por usuarios del sistema</b></h2>
                     </div>
+                </div>
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Usuario</th>
+                                <th>Acción</th>
+                                <th>Fecha</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>John Doe Doe john@example.com</td>
+                                <td>Creo xxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxx  xxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxx</td>
+                                <td>Tue Sep 28 20:37:25 2021</td>
+                            </tr>
+                            <tr>
+                                <td>Marie Doe Doe john@example.com</td>
+                                <td>Edito xxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxx  xxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxx</td>
+                                <td>Tue Sep 28 20:37:25 2021</td>
+                            </tr>
+                            <tr>
+                                <td>Pepe Doe Doe john@example.com</td>
+                                <td>Elimino xxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</td>
+                                <td>Tue Sep 28 20:37:25 2021</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
@@ -25,3 +45,7 @@
 </div>
 
 @endsection
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+e
