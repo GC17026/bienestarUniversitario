93d9d3ffc027f055f5c2c14c2bfec6813e007173
GC17026 <GC17026@ues.edu.sv>
2021-10-07 23:34:20 -0600

    crud contenido

--- app/Http/Controllers/ContenidoController.php
+++ app/Http/Controllers/ContenidoController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Contenido;
 use Illuminate\Http\Request;
+use Response;
+use App\Http\Controllers\Throwable;
+use App\SubSeccion;
+use App\Seccion;
+use Illuminate\Support\Facades\Storage;
 
 class ContenidoController extends Controller
 {
@@ -35,7 +40,38 @@ class ContenidoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $contenido = new Contenido;
+            $contenido->titulo = $request->titulo;
+            $contenido->contenido = $request->contenido;
+            if ($request->contenidoType == "seccion") {
+                $contenido->seccion_id = $request->seccionPadre;
+                if (Count(Seccion::find($request->seccionPadre)->contenidos) < 5) {
+                    $contenido->seccion_id = $request->seccionPadre;
+                } else {
+                    return Response::json(['error' => 'Solamente se pueden agregar 5 contenidos por seccion'], 400);
+                }
+            } else {
+                $contenido->sub_seccion_id = $request->seccionPadre;
+                if (Count(SubSeccion::find($request->seccionPadre)->contenidos) < 5) {
+                    $contenido->seccion_id = $request->seccionPadre;
+                } else {
+                    return Response::json(['error' => 'Solamente se pueden agregar 5 contenidos por subseccion'], 400);
+                }
+            }
+
+            if ($request->hasFile('foto_contenido')) {
+                $file = $request->file('foto_contenido');
+                $extension = $file->getClientOriginalExtension();
+                $filename = time() . '1.' . $extension;
+                $contenido->urlImg = '/public/uploads/' . $filename;
+                $file->move('public/uploads/', $filename);
+            }
+            $contenido->save();
+            return Response::json(['success' => 'Se ha agregado un nuevo contenido'], 200);
+        } catch (Exception $e) {
+            return Response::json(['error' => 'Ocurrió un error, es posible que el formato de la imagen no sea permitido'], 400);
+        }
     }
 
     /**
@@ -67,9 +103,26 @@ class ContenidoController extends Controller
      * @param  \App\Contenido  $contenido
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Contenido $contenido)
+    public function update(Request $request)
     {
-        //
+        try{
+            $contenido = Contenido::find($request->id);
+            if ($request->hasFile('foto_contenido')) {
+                unlink(trim(getcwd() . $contenido->urlImg));
+                $file = $request->file('foto_contenido');
+                $extension = $file->getClientOriginalExtension();
+                $filename = time() . '1.' . $extension;
+                $contenido->urlImg = '/public/uploads/' . $filename;
+                $file->move('public/uploads/', $filename);
+            }
+            $contenido->titulo=$request->titulo;
+            $contenido->contenido=$request->contenido;
+            $contenido->save();
+            return Response::json(['success' => 'Se ha actualizado un contenido'], 200);
+        }
+        catch (Exception $e) {
+            return Response::json(['error' => 'Ocurrió un error, es posible que el formato de la imagen no sea permitido'], 400);
+        }
     }
 
     /**
@@ -78,8 +131,16 @@ class ContenidoController extends Controller
      * @param  \App\Contenido  $contenido
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Contenido $contenido)
+    public function destroy(Request $request)
     {
-        //
+        try{
+            $contenido = Contenido::find($request->toDeleteId);
+            unlink(trim(getcwd() . $contenido->urlImg));
+            $contenido->delete();
+            return Response::json(['success' => 'Se ha eliminado el contenido con éxito'], 200);
+        }
+        catch (Exception $e) {
+            return Response::json(['error' => 'Ocurrió un error, es posible que el formato de la imagen no sea permitido'], 400);
+        }
     }
 }
